a24ef00318e4e48c898b294d7c5aba0211481b8d
zhangpengfei <18409271159@163.com>
2024-12-09 11:56:10 +0800

    feat: write login page and interface

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -10,3 +10,9 @@ export async function register(username: string, password: string) {
         username, password
     })
 }
+
+export async function login(username: string, password: string) {
+    return await axiosInstance.post('/user/login', {
+        username, password
+    })
+}
