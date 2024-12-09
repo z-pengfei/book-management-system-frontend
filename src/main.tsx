93bf9645dafc6261496e2f6157ecf972e0908067
zhangpengfei <18409271159@163.com>
2024-12-09 10:55:40 +0800

    feat: add route pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,34 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import ReactDOM from 'react-dom/client'
+import { RouterProvider,createBrowserRouter } from 'react-router-dom'
+import { Login } from './pages/Login'
+import { Register } from './pages/Register'
+import { BookManage } from './pages/BookManage'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const routes = [
+  {
+    path: '/login',
+    element:<Login/>
+  },
+  {
+    path: '/register',
+    element: <Register/>
+  },
+  {
+    path: '/',
+    element: <BookManage/>
+  }
+]
+
+const router = createBrowserRouter(routes);
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+
+root.render(<RouterProvider router={router}/>)
+
+// createRoot(document.getElementById('root')!).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
